3e536cf30984bfb08099442c9a43b5c43619ecba
James Carlson <jxxcarlson@mac.com>
2015-10-13 13:16:19 -0400

    add comments

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,7 @@
+get '/', to: 'home#index'
+
 get '/documents', to: 'documents#index'
+
 # Configure your routes here
 # See: http://www.rubydoc.info/gems/lotus-router/#Usage
 
-get '/', to: 'home#index'
--- lib/noteshare/entities/document.rb
+++ lib/noteshare/entities/document.rb
@@ -3,6 +3,8 @@ class Document
   attributes :id, :author, :title, :tags, :meta,
     :createdAt, :modifiedAt, :content, :part
 
+  # *section.add_to(doc)* appends the id of *section*
+  # to the array *doc.part*
   def add_to(parent_document)
     DocumentRepository.persist(self) unless self.id
     if parent_document
@@ -13,10 +15,14 @@ class Document
     DocumentRepository.persist(parent_document)
   end
 
+  # *doc.subdocment(k)* returns the k-th
+  # subdocument of *doc*
   def subdocument(k)
     DocumentRepository.find(part[k])
   end
 
+  # *doc.subdocument_titles* returns a list of the
+  # titles of the sections of *document*.
   def subdocument_titles
     list = []
     part.each do |id|
@@ -25,7 +31,14 @@ class Document
     end
     list
   end
-  
+
+  # *doc.compile* concatenates the contents
+  # of *doc* with the compiled text of
+  # each section of *doc*.  The sections
+  # array is determined by #part, which is
+  # a persistent array of integers which
+  # represent the id's of the sections of
+  # *doc*.
   def compile
     compiled_text = self.content || 'Yo! '
     part.each do |id|
