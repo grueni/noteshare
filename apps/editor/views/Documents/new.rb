1b98e16301e2a26af03dd492d41b6db05da62bc4
James Carlson <jxxcarlson@mac.com>
2015-10-24 16:32:51 -0400

    Use form_for in View to construct new document form.

--- apps/editor/views/Documents/new.rb
+++ apps/editor/views/Documents/new.rb
@@ -1,8 +1,16 @@
 module Editor::Views::Documents
   class New
     include Editor::View
-    def foo
-      'FND'
+
+    def form
+      
+      form_for :document, '/documents' do
+        text_field :title
+        text_field :author
+
+        submit 'Create'
+      end
     end
+
   end
 end
