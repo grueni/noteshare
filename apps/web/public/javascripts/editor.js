7189bd6897021b41942fd69d1eb273f93ffa1faf
James Carlson <jxxcarlson@mac.com>
2015-10-31 05:26:32 -0400

    Ajax update of edited text now working.

--- apps/web/public/javascripts/editor.js
+++ apps/web/public/javascripts/editor.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
     console.log('status: ' + status);
     console.log('DATA:' + data  );
 
-    $('#rendered_content').html = data;     
+    $('#rendered_content').html(data);
 
-    console.log('status:2 ' + status);
   }
+  
 
   function udpate_document() {
 
@@ -19,8 +19,8 @@ $(document).ready(function(){
     var element2 = document.getElementById('document-document-id');
     var id = element2.value;
 
-    console.log('I will post for id: ' + id);
-    console.log('I will post this: ' + source_text);
+    //console.log('I will post for id: ' + id);
+    //console.log('I will post this: ' + source_text);
 
     $.post( 'http://localhost:2300/editor/json_update/' + id, { source: source_text}, update_rendered_content );
   }
