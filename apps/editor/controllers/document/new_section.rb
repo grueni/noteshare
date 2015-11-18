6a04d5f003b6a30a2eb2424f1b7b4082d2aafb42
James Carlson <jxxcarlson@mac.com>
2015-11-18 16:06:59 -0500

    Materials and wiring for creating a new section:

--- apps/editor/controllers/document/new_section.rb
+++ apps/editor/controllers/document/new_section.rb
@@ -2,7 +2,16 @@ module Editor::Controllers::Document
   class NewSection
     include Editor::Action
 
+    expose :document
+    expose :parent_document
+
     def call(params)
+
+      @document = DocumentRepository.find params[:id]
+      @parent_document = @document.parent_document
+
+
     end
+
   end
 end
--- apps/editor/views/document/new_section.rb
+++ apps/editor/views/document/new_section.rb
@@ -1,5 +1,24 @@
 module Editor::Views::Document
   class NewSection
     include Editor::View
+
+    def new_section_form
+      form_for :document, '/editor/create_new_section' do
+        label :title
+        text_field :title
+
+        label :options
+        text_field :options
+
+        label :content
+        text_area :content, {style: 'height:200px;'}
+
+        hidden_field :parent_id, value: params[:id]
+
+        submit 'Create', {style: 'margin-top:1em;'}
+        # button_to 'Cancel', "/documents/#{params[:id]}"
+      end
+    end
+
   end
 end
