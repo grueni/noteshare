2b1aa629f791ccdeb23b2d0f3cead99e20b50150
James Carlson <jxxcarlson@gmail.com>
2015-11-22 07:09:18 -0500

    css, about page text

--- apps/node/application.rb
+++ apps/node/application.rb
@@ -178,7 +178,9 @@ module Node
       #  * http://content-security-policy.com/
       #  * https://developer.mozilla.org/en-US/docs/Web/Security/CSP/Using_Content_Security_Policy
       #
-      security.content_security_policy "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; font-src 'self';"
+
+      # Fixme: commented out temporarily by JC
+      # security.content_security_policy "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; font-src 'self';"
 
       ##
       # FRAMEWORKS
--- apps/session_manager/application.rb
+++ apps/session_manager/application.rb
@@ -179,7 +179,8 @@ module SessionManager
       #  * http://content-security-policy.com/
       #  * https://developer.mozilla.org/en-US/docs/Web/Security/CSP/Using_Content_Security_Policy
       #
-      security.content_security_policy "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; font-src 'self';"
+      # Fixme: temporarily commeneted out by JC
+      # security.content_security_policy "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; font-src 'self';"
 
       ##
       # FRAMEWORKS
