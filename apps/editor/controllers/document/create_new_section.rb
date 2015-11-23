554aa1ca1c6731f665d5dcfb089840e33a1bc91c
James Carlson <jxxcarlson@gmail.com>
2015-11-22 22:21:45 -0500

    better coordination of associated doc with master doc

--- apps/editor/controllers/document/create_new_section.rb
+++ apps/editor/controllers/document/create_new_section.rb
@@ -11,17 +11,25 @@ module Editor::Controllers::Document
       content = document_packet['content']
       parent_id = document_packet['parent_id']
 
+      user = current_user(session)
+
+      puts "user: #{user.screen_name}".magenta
+
+      puts "options: #{options}".magenta
+      puts "parent_id: #{parent_id}".magenta
+
       if options
         option_hash = options.hash_value || {}
       end
 
       associated_doc_type = option_hash['associate_as'] || ''
-      author = UserRepository.find current_user(session).id
+      author = UserRepository.find user.id
 
       current_document = DocumentRepository.find session[:current_document_id]
       current_root_document = current_document.root_document
       new_document = NSDocument.create(title: title, content: content, author_credentials: author.credentials)
 
+      puts "associated_doc_type: #{associated_doc_type}".red
 
       if associated_doc_type == ''
         new_document.add_to(current_root_document)
@@ -29,8 +37,13 @@ module Editor::Controllers::Document
         new_document.associate_to(current_document, associated_doc_type,)
       end
 
-      node = NodeRepository.find current_user.node_id
-      node.update_docs_for_owner
+      node = NSNodeRepository.find user.node_id
+      if node
+        node.update_docs_for_owner
+      else
+        puts "No node for user #{user.screen_name}".magenta
+      end
+
 
       redirect_to "/document/#{new_document.id}"
 
