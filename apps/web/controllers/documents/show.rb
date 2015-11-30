a8ff4910c7635aa9bcc4f2943859f9db39b32e53
James Carlson <jxxcarlson@gmail.com>
2015-11-29 22:08:59 -0500

    x

--- apps/web/controllers/documents/show.rb
+++ apps/web/controllers/documents/show.rb
@@ -8,14 +8,18 @@ module Web::Controllers::Documents
 
 
     def call(params)
+
       @document = DocumentRepository.find(params['id'])
+      puts @document.is_root_document?
+
       session[:current_document_id] = @document.id
+=begin
       if @document.is_root_document?
         @document.compile_with_render
       else
         @document.update_content
       end
+=end
     end
-
   end
 end
