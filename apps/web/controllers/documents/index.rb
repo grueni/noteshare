392972961634803383f19fc2ce06e8a0292c2c7a
James Carlson <jxxcarlson@mac.com>
2015-12-21 08:09:49 -0500

    Filter search hits use the can_read lambda

--- apps/web/controllers/documents/index.rb
+++ apps/web/controllers/documents/index.rb
@@ -5,16 +5,20 @@
     expose :documents, :nodes
     expose :active_item
 
+    def yono(arg) false end
+
+
     def call(params)
 
       # @documents = DocumentRepository.root_documents.sort_by { |item| item.title }
-      @documents = DocumentRepository.root_documents
+      user = current_user(session)
+      # @documents = DocumentRepository.root_documents.all.select{ |doc| Permission.new(user, :read, doc).grant }.sort_by { |item| item.title }
+      @documents = DocumentRepository.root_documents.all.select &can_read(user)
       @active_item = 'documents'
-
 
       @nodes = NSNodeRepository.all
     end
 
-
   end
+
 end
