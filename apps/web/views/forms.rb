9a8f03181e7dd772a4851909e7e69004a4ec4b75
James Carlson <jxxcarlson@mac.com>
2015-12-28 15:21:58 -0500

    css iphone

--- apps/web/views/forms.rb
+++ apps/web/views/forms.rb
@@ -7,13 +7,19 @@ module Web
 
       def basic_search_form
         form_for :search, '/search' do
-          text_field :search, id: 'basic_search_form'
+          text_field :search, {id: 'basic_search_form'}
         end
       end
 
       def basic_search_form_short
         form_for :search, '/search' do
           text_field :search, {style: 'position:absolute; top:-6px; left:300px;; padding-left: 20px; placeholder: Search for; color: white; background-color: #444; height: 28px; width:180px;'}
+        end
+      end
+
+      def small_search_form
+        form_for :search, '/search' do
+          text_field :search, {style: 'position:absolute; top:-6px; left:40px; padding-left: 20px; placeholder: Search for; color: white; background-color: #444; height: 28px; width:100px;'}
         end
       end
 
