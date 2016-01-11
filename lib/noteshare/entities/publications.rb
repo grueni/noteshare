22791af831a2b0d5140abd3033d8956cdfcbc6af
James Carlson <jxxcarlson@gmail.com>
2016-01-11 10:48:20 -0500

    Use join table to model publications

--- lib/noteshare/entities/publications.rb
+++ lib/noteshare/entities/publications.rb
@@ -1,7 +1,12 @@
+# require '../repositories/publications_repository'
+
+
 class Publications
   include Lotus::Entity
+
   attributes :id, :node_id, :document_id
 
+  # for transiton only
   def self.add_documents_for_node(node)
     node.documents_as_hash.each do |title, doc_id|
       publication = Publications.new(node_id: node.id, document_id: doc_id)
@@ -10,13 +15,51 @@ class Publications
     end
   end
 
-  def self.for_node(node)
-    PubicationsRepository.documents_for_node(node)
+  def document
+    DocumentRepository.find document_id
   end
 
-  def self.for_document(document)
-    PubicationsRepository.nodes_for_document(document)
+  def node
+    NSNodeRepository.find node_id
   end
+
+  # Add publication record with given node_id and docuemnt_id
+  # unless one of those id's is invalid
+  def self.add(node_id, document_id)
+    publication = Publications.new(node_id: node_id, document_id: document_id)
+    return if NSNodeRepository.find node_id == nil
+    return if DocumentRepository.find document_id == nil
+    PublicationsRepository.create publication
+  end
+
+  def self.find_for_pair(node_id, document_id)
+    PublicationsRepository.record_for_pair(node_id, document_id)
+  end
+
+  def self.remove(node_id, document_id)
+    publication = PublicationsRepository.find_for_pair(node_id, document_id)
+    PublicationsRepository.create publication if publication
+  end
+
+  def self.for_node(node_id)
+    p = PublicationsRepository.records_for_node(node_id)
+    docs = []
+    p.all.each do |record|
+      docs << record.document
+    end
+    docs
+  end
+
+  def self.for_document(document_id)
+    n = PublicationsRepository.records_for_document(document_id)
+    nodes = []
+    n.all.each do |record|
+      nodes << record.node
+    end
+    nodes
+  end
+
+
 
 
 end
--- lib/noteshare/repositories/publications_repository.rb
+++ lib/noteshare/repositories/publications_repository.rb
@@ -1,34 +1,22 @@
 class PublicationsRepository
   include Lotus::Repository
 
-  def self.for_node(node)
-    name = node.class.name
-    case name
-      when 'NSNode'
-        node_id = node.id
-      when 'String'
-        node_id = node.to_i
-      else
-        node_id = node # assume integer
-    end
+  def self.records_for_node(node_id)
     query do
       where(node_id: node_id)
     end
   end
 
-  def self.for_document(document)
-    name = document.class.name
-    case name
-      when 'NSDocument'
-        document_id = document.id
-      when 'String'
-        document_id = document.to_i
-      else
-        document_id = document # assume integer
-    end
+  def self.records_for_document(document_id)
     query do
       where(document_id: document_id)
     end
+  end
+
+  def self.record_for_pair(node_id, document_id)
+    query do
+      where(document_id: document_id, node_id: node_id)
+    end.first
   end
 
 end
