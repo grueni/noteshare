0c5505ec7b0a70d1c732c14048d7b1b196946050
James Carlson <jxxcarlson@gmail.com>
2016-01-12 13:58:21 -0500

    Autosave implemented with interval of 2.5 seconds

--- apps/web/public/javascripts/editor.js
+++ apps/web/public/javascripts/editor.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
   $('#update_source_button').click(update_document);
 
-  window.setInterval(auto_update_document, 5000);
+  window.setInterval(auto_update_document, 2500);
 
 
 
@@ -15,7 +15,7 @@ update_rendered_content = function(data, status) {
 
   $('#rendered_content').html(data);
 
-  console.log('update_rendered_content');
+  // console.log('update_rendered_content');
 
   reloadMathJax();
 
@@ -24,30 +24,29 @@ update_rendered_content = function(data, status) {
 
 auto_update_document = function () {
 
-  console.log('\n------\nAuto_update_document');
+  // console.log('\n------\nAuto_update_document');
 
   var element = document.getElementById('document-updated-text');
   var source_text = element.value;
-
-  console.log('source: ' + source_text.slice(0,20));
-  local_storage = localStorage.getItem('source')
-  if (local_storage == null) {
-    console.log('local storage is NULL');
-  } else {
-    console.log('local storage: ' + local_storage.slice(0,20));
-  }
-  localStorage.setItem('source', source_text)
-
   var element2 = document.getElementById('document-document-id');
   var id = element2.value;
 
+  // console.log('source: ' + source_text.slice(0,20));
+  local_storage = localStorage.getItem('source')
+  if (local_storage == null) {
+    // console.log('local storage is NULL');
+    localStorage.setItem('source', source_text)
+  } else if (local_storage != source_text) {
+    // console.log('local storage: ' + local_storage.slice(0,20));
+    $.post( '/editor/json_update/' + id, { source: source_text }, update_rendered_content );
+    localStorage.setItem('source', source_text);
+  }
 
-  $.post( '/editor/json_update/' + id, { source: source_text }, update_rendered_content );
 }
 
 update_document = function () {
 
-  console.log('Update_document');
+  // console.log('Update_document');
 
   var element = document.getElementById('document-updated-text');
   var source_text = element.value;
@@ -55,7 +54,7 @@ update_document = function () {
   var element2 = document.getElementById('document-document-id');
   var id = element2.value;
 
-  console.log('update_document with id = ' + id);
+  // console.log('update_document with id = ' + id);
   // console.log('update_document with csrf_token ' + csrfToken()); /* DANGER */
 
 
