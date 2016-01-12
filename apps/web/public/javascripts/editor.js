c36a381af203022b58b0f7234247cc745a702c03
James Carlson <jxxcarlson@gmail.com>
2016-01-12 13:52:05 -0500

    logging source text to local storage for comparison

--- apps/web/public/javascripts/editor.js
+++ apps/web/public/javascripts/editor.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
   $('#update_source_button').click(update_document);
 
-  window.setInterval(auto_update_document, 10000);
+  window.setInterval(auto_update_document, 5000);
 
 
 
@@ -24,16 +24,22 @@ update_rendered_content = function(data, status) {
 
 auto_update_document = function () {
 
-  console.log('ENTER: update_document');
+  console.log('\n------\nAuto_update_document');
 
   var element = document.getElementById('document-updated-text');
   var source_text = element.value;
 
+  console.log('source: ' + source_text.slice(0,20));
+  local_storage = localStorage.getItem('source')
+  if (local_storage == null) {
+    console.log('local storage is NULL');
+  } else {
+    console.log('local storage: ' + local_storage.slice(0,20));
+  }
+  localStorage.setItem('source', source_text)
+
   var element2 = document.getElementById('document-document-id');
   var id = element2.value;
-
-  console.log('update_document with id = ' + id);
-  // console.log('update_document with csrf_token ' + csrfToken()); /* DANGER */
 
 
   $.post( '/editor/json_update/' + id, { source: source_text }, update_rendered_content );
@@ -41,7 +47,7 @@ auto_update_document = function () {
 
 update_document = function () {
 
-  console.log('ENTER: update_document');
+  console.log('Update_document');
 
   var element = document.getElementById('document-updated-text');
   var source_text = element.value;
