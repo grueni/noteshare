2f566b90cddd4832a4d8ad1211f5ac7463ae7d6e
James Carlson <jxxcarlson@gmail.com>
2016-01-12 09:05:56 -0500

    Use Publications table (join nodes and users)

--- lib/noteshare/entities/publications.rb
+++ lib/noteshare/entities/publications.rb
@@ -9,9 +9,12 @@ class Publications
   # for transiton only
   def self.add_documents_for_node(node)
     node.documents_as_hash.each do |title, doc_id|
-      publication = Publications.new(node_id: node.id, document_id: doc_id)
-      puts "#{title} => #{node.name}".red
-      PublicationsRepository.create publication
+      publication = Publication.find_for_pair(node.id, doc_id)
+      if publication.nil?
+        publication = Publications.new(node_id: node.id, document_id: doc_id)
+        puts "#{title} => #{node.name}".red
+        PublicationsRepository.create publication
+      end
     end
   end
 
@@ -19,17 +22,24 @@ class Publications
     DocumentRepository.find document_id
   end
 
+  def document_title
+    document.title
+  end
+
   def node
     NSNodeRepository.find node_id
   end
 
   # Add publication record with given node_id and docuemnt_id
-  # unless one of those id's is invalid
-  def self.add(node_id, document_id)
-    publication = Publications.new(node_id: node_id, document_id: document_id)
-    return if NSNodeRepository.find node_id == nil
-    return if DocumentRepository.find document_id == nil
-    PublicationsRepository.create publication
+  # unless one of those id's is invalid, e.g., not present
+  def self.add_record(node_id, document_id)
+    return if (NSNodeRepository.find node_id) == nil
+    return if (DocumentRepository.find document_id) == nil
+    record = self.find_for_pair(node_id,document_id)
+    if record == nil
+      publication = Publications.new(node_id: node_id, document_id: document_id)
+      PublicationsRepository.create publication
+    end
   end
 
   def self.find_for_pair(node_id, document_id)
@@ -41,7 +51,11 @@ class Publications
     PublicationsRepository.create publication if publication
   end
 
-  def self.for_node(node_id)
+  def self.records_for_node(node_id)
+    PublicationsRepository.records_for_node(node_id)
+  end
+
+  def self.documents_for_node(node_id)
     p = PublicationsRepository.records_for_node(node_id)
     docs = []
     p.all.each do |record|
@@ -50,7 +64,7 @@ class Publications
     docs
   end
 
-  def self.for_document(document_id)
+  def self.nodes_for_document(document_id)
     n = PublicationsRepository.records_for_document(document_id)
     nodes = []
     n.all.each do |record|
