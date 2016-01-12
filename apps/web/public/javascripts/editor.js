20ca4b30daa276853bbba6e3238d25bd240b9578
James Carlson <jxxcarlson@gmail.com>
2016-01-12 13:42:46 -0500

    Use separate update and autoupdate functions

--- apps/web/public/javascripts/editor.js
+++ apps/web/public/javascripts/editor.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
   $('#update_source_button').click(update_document);
 
-  window.setInterval(update_document, 10000);
+  window.setInterval(auto_update_document, 10000);
 
 
 
@@ -21,6 +21,23 @@ update_rendered_content = function(data, status) {
 
 }
 
+
+auto_update_document = function () {
+
+  console.log('ENTER: update_document');
+
+  var element = document.getElementById('document-updated-text');
+  var source_text = element.value;
+
+  var element2 = document.getElementById('document-document-id');
+  var id = element2.value;
+
+  console.log('update_document with id = ' + id);
+  // console.log('update_document with csrf_token ' + csrfToken()); /* DANGER */
+
+
+  $.post( '/editor/json_update/' + id, { source: source_text }, update_rendered_content );
+}
 
 update_document = function () {
 
