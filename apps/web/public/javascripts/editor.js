89a05b7ac9d2d8ddc3a5b13a53114dd71d2e2424
James Carlson <jxxcarlson@gmail.com>
2016-01-12 11:15:52 -0500

    experimeinting on auto-update

--- apps/web/public/javascripts/editor.js
+++ apps/web/public/javascripts/editor.js
@@ -1,34 +1,38 @@
 $(document).ready(function(){
 
 
-  function update_rendered_content(data, status) {
 
-    $('#rendered_content').html(data);
-
-    console.log('update_rendered_content');
-
-    reloadMathJax();
-
-  }
-
-
-  function update_document() {
-
-    var element = document.getElementById('document-updated-text');
-    var source_text = element.value;
-
-    var element2 = document.getElementById('document-document-id');
-    var id = element2.value;
-
-    console.log('update_document with id = ' + id);
-    // console.log('update_document with csrf_token ' + csrfToken()); /* DANGER */
-
-
-    $.post( '/editor/json_update/' + id, { source: source_text }, update_rendered_content );
-  }
-
-  $('#update_source_button').click(update_document);
+  $('#update_source_button').click($.update_document);
 
 
 
 });
+
+
+$.update_rendered_content = function(data, status) {
+
+  $('#rendered_content').html(data);
+
+  console.log('update_rendered_content');
+
+  reloadMathJax();
+
+}
+
+
+$.update_document = function () {
+
+  console.log('$.update_document');
+
+  var element = document.getElementById('document-updated-text');
+  var source_text = element.value;
+
+  var element2 = document.getElementById('document-document-id');
+  var id = element2.value;
+
+  console.log('update_document with id = ' + id);
+  // console.log('update_document with csrf_token ' + csrfToken()); /* DANGER */
+
+
+  $.post( '/editor/json_update/' + id, { source: source_text }, $.update_rendered_content );
+}
