1d9a63812f3ec5a5e1f5f03c9e6c8dc6f16ee93b
James Carlson <jxxcarlson@gmail.com>
2016-01-13 13:02:44 -0500

    logging statements for autorefresh

--- apps/web/public/javascripts/editor.js
+++ apps/web/public/javascripts/editor.js
@@ -13,6 +13,8 @@ $(document).ready(function(){
 
 update_rendered_content = function(data, status) {
 
+  console.log('update_rendered_content');
+
   $('#rendered_content').html(data);
 
   // console.log('update_rendered_content');
@@ -24,20 +26,20 @@ update_rendered_content = function(data, status) {
 
 auto_update_document = function () {
 
-  // console.log('\n------\nAuto_update_document');
+  console.log('Auto_update_document');
 
   var element = document.getElementById('document-updated-text');
   var source_text = element.value;
   var element2 = document.getElementById('document-document-id');
   var id = element2.value;
 
-  // console.log('source: ' + source_text.slice(0,20));
+  console.log('source: ' + source_text.length);
   local_storage = localStorage.getItem('source')
   if (local_storage == null) {
-    // console.log('local storage is NULL');
+    console.log('local storage is NULL');
     localStorage.setItem('source', source_text)
   } else if (local_storage != source_text) {
-    // console.log('local storage: ' + local_storage.slice(0,20));
+    console.log('local storage: ' + local_storage.length);
     $.post( '/editor/json_update/' + id, { source: source_text }, update_rendered_content );
     localStorage.setItem('source', source_text);
     $('#count_words').html("<span>" + count_words(source_text) + "</span>")
@@ -47,7 +49,7 @@ auto_update_document = function () {
 
 update_document = function () {
 
-  // console.log('Update_document');
+  console.log('Update_document');
 
   var element = document.getElementById('document-updated-text');
   var source_text = element.value;
