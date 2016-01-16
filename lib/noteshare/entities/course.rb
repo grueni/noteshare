1e50cde26c8c03ecd907e5f9bce8de63744ea1e9
James Carlson <jxxcarlson@gmail.com>
2016-01-16 10:44:19 -0500

    Remove logging statements from create_master_document and related code.

--- lib/noteshare/entities/course.rb
+++ lib/noteshare/entities/course.rb
@@ -40,51 +40,25 @@ class Course
     master.content ||= ''
     lessons = self.associated_lessons
     lesson_count = lessons.count
-    puts "Lesson to import: #{lesson_count}".red
-
-    puts "A".red
-
-    mac = master.author_credentials
-    puts mac.inspect.red
-
-    puts "A2".red
+    puts "Lessons to import: #{lesson_count}".red
 
     _tex_macros = tex_macros
     if _tex_macros
-      puts "-- a1".cyan
       tex_macro_document =  NSDocument.create(title: 'Tex Macros', author_credentials: JSON.parse(master.author_credentials))
-      puts "-- a2".cyan
       tex_macro_document.content = "\\(" + _tex_macros + "\\)"
-      puts "-- a3".cyan
       DocumentRepository.update tex_macro_document
-      puts "-- a4".cyan
       tex_macro_document.associate_to(master, 'texmacros')
     end
 
-    puts "B".red
-
     stack = []
     last_node = master
-
-    puts "C".red
-
     count = 0
-    lessons.all.each do |lesson|
 
+    lessons.all.each do |lesson|
       count = count + 1
       puts "#{count}: #{lesson.id}, #{lesson.title}".cyan
-
       begin
-
-        puts "before 'to_document'".red
-
         section = lesson.to_document(screen_name)
-
-        puts "section.asciidoc_level; #{section.asciidoc_level}".green
-
-        #if lesson.aside and lesson.aside.length > 1
-        #  section.add_associate(title: 'Notes', content: lesson.aside, type: 'aside')
-        # end
         stack == [] ?  delta = 2 : delta =  section.asciidoc_level - stack.last.asciidoc_level
         if delta >= 2
           stack.push(last_node)
@@ -93,51 +67,11 @@ class Course
         end
         section.add_to(stack.last)
         last_node = section
-
-        puts "  -- ok".blue
-
       rescue
-
         puts "Error in importing #{lesson.title} (#{lesson.id})".red
-
       end
-
-
-
-    end
-    return master
-  end
-
-  def create_master_document1(screen_name)
-
-    master = self.to_document(screen_name)
-
-    master.content ||= ''
-    lessons = self.associated_lessons
-    lesson_count = lessons.count
-
-    _tex_macros = tex_macros
-    if _tex_macros
-      tex_macro_document =  NSDocument.create(title: 'Tex Macros', author_credentials: master.author_credentials)
-      tex_macro_document.content = "\\(" + _tex_macros + "\\)"
-      DocumentRepository.update tex_macro_document
-      tex_macro_document.associate_to(master, 'texmacros')
     end
 
-    stack = []
-    last_node = master
-
-    lessons.all.each do |lesson|
-      section = lesson.to_document(screen_name)
-      stack == [] ?  delta = 2 : delta =  section.asciidoc_level - stack.last.asciidoc_level
-      if delta >= 2
-        stack.push(last_node)
-      elsif delta <= 0
-        x = stack.pop
-      end
-      section.add_to(stack.last)
-      last_node = section
-    end
     return master
   end
 
--- lib/noteshare/entities/lesson.rb
+++ lib/noteshare/entities/lesson.rb
@@ -4,37 +4,19 @@ class Lesson
              :tags, :area, :sequence, :summary, :aside, :image_path1, :image_path2, :content_type
 
   def handle_pdf(document)
-    puts "ENTER: handle_pdf".magenta
-    puts "image_path1 is #{self.image_path1}"
     document.dict['pdf:image_id'] = self.image_path1
     image = ImageRepository.find self.image_path1
-    if image
-      puts "image found: #{image.url2}".cyan
-    else
-      puts "image not found".red
-    end
     return if image == nil
-    puts "image in is not nil, proceeding".cyan
     document.dict['pdf:url'] = image.url2
-    puts "dict has been updated: #{document.dict}".green
-    puts "document content = #{document.content}".red
     document.content = self.content || ''
-    puts "A".red
     document.content << "\n\n#{image.url2}[PDF document]\n\n"
-    puts "B".red
     document.content << "++++\n"
     document.content << "<iframe src='#{image.url2}' width=100% height=1200 ></iframe>\n"
-    puts "C".red
     document.content << "++++\n"
-    puts "content has been updated".red
   end
 
-  def handle_pdf1(document)
-    puts "HAS PDF: #{document.title}"
-  end
 
   def to_document(screen_name)
-    puts "In to_document for #{self.title} (#{self.id})".red
     user = UserRepository.find_one_by_screen_name(screen_name)
     return if user == nil
     doc = NSDocument.create(title: self.title, author_credentials: user.credentials)
@@ -46,10 +28,8 @@ class Lesson
     doc.content = self.content
 
     handle_pdf(doc) if self.content_type == 'pdf'
-    puts "I have returned from handle_pdf".magenta  if self.content_type == 'pdf'
 
     DocumentRepository.update(doc)
-    puts "   --- doc content length: #{doc.content.length}".magenta
     return doc
   end
 
