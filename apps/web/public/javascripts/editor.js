c3311262ead48e62fd380e617e7b81816c0ff53a
James Carlson <jxxcarlson@mac.com>
2016-01-23 06:02:26 -0700

    Add comments to editor.js

--- apps/web/public/javascripts/editor.js
+++ apps/web/public/javascripts/editor.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 
-
+  /* Do manual update/refresh */
   $('#update_source_button').click(update_document);
 
   refresh_auto_updater = setInterval(auto_update_document, 3000);
@@ -12,6 +12,8 @@ $(document).ready(function(){
     max: 100
   });
 
+
+  /* The slider sets the auto-refresh interval; the initial value is 3 seconds. */
   $('#slider_value').html("<span>auto-refresh: " + 3 + " seconds</span>")
 
   $( "#slider" ).slider({
@@ -29,6 +31,7 @@ $(document).ready(function(){
 });
 
 
+/* Replace the rendered content with the updated rendered content */
 update_rendered_content = function(data, status) {
 
   $('#rendered_content').html(data);
@@ -37,7 +40,7 @@ update_rendered_content = function(data, status) {
 
 }
 
-// Delay ranges from 0.5 seconds to 1 minute 0.5 seconds
+// Get the auto-upodate delay; it ranges from 0.5 seconds to 1 minute 0.5 seconds
 auto_update_delay = function() {
 
   var val = $('#slider').slider("option", "value");
@@ -47,18 +50,19 @@ auto_update_delay = function() {
 
 }
 
-
+/*  auto_update_document stores the last-used version
+ of the edited text.  It compares this with the currnt
+ text.  If there are different, then we POST to
+ /editor/json_update/' to update the text and its
+ rendered verson. See the notes below for 'update_document'.
+ */
 auto_update_document = function () {
 
-
-
-    localStorage;
 
   var element = document.getElementById('document-updated-text');
   var source_text = element.value;
   var element2 = document.getElementById('document-document-id');
   var id = element2.value;
-  /****** GGG *****/
   var local_source_text  =  localStorage.getItem('local_source_text');
 
   if (local_source_text == null) {
@@ -88,6 +92,17 @@ auto_update_document = function () {
 
 }
 
+/* Send the updated text to the server by POST.
+The action '/editor/json_update/' will render
+the Asciidoc text received as HTML and place
+it in data using the statement
+
+ self.body = @document.rendered_content
+
+The callback 'update_rendered_content'
+will then use this HTML to update the rendered
+text in the client's browser.
+ */
 update_document = function () {
 
   console.log('Update_document');
