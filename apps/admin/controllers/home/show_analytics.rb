b43a2263e1008066eb586eb82df2d1be1859b495
James Carlson <jxxcarlson@mac.com>
2016-01-23 00:31:50 -0700

    Simplify analytics reporting and add report on edits.

--- apps/admin/controllers/home/show_analytics.rb
+++ apps/admin/controllers/home/show_analytics.rb
@@ -4,40 +4,35 @@ module Admin::Controllers::Home
   class ShowAnalytics
     include Admin::Action
 
-    expose :active_item, :analytics_pages_signed_in, :analytics_pages_anonymous,
-           :analytics_sign_ins, :analytics_sign_outs, :analytics_sign_ups, :analytics_unauth_access_attempts
+    expose :active_item, :doc_views_signed_in, :doc_views_anonymous, :edits,
+           :sign_ins, :sign_outs, :sign_ups, :unauth_access_attempts
     def call(params)
-
-      puts "Analytics,  controller".red
 
       redirect_if_not_admin('Attempt to show analytics (admin, home, show analytics)')
 
       puts "Here is controller DisplayAnalytics".magenta
       @active_item = 'admin'
 
-      page_view_count_signed_in = Analytics.get_keen_data(query_type: 'count', collection: 'document_views_signed_in')
-      page_view_average_signed_in = Analytics.get_keen_data(query_type: 'average', collection: 'document_views_signed_in')
-      @analytics_pages_signed_in = "Page views, signed in: #{page_view_count_signed_in}, average: #{page_view_average_signed_in}"
+      document_view_count_signed_in = Analytics.get_keen_data(query_type: 'count', collection: 'document_views_signed_in')
+      @doc_views_signed_in = "Document views, signed in: #{document_view_count_signed_in}"
 
-      page_view_count_anonymous = Analytics.get_keen_data(query_type: 'count', collection: 'anonymous_document_views')
-      anonymous_document_views = Analytics.get_keen_data(query_type: 'average', collection: 'anonymous_document_views')
-      @analytics_pages_anonymous = "Page views, anonymous: #{page_view_count_anonymous}, average: #{anonymous_document_views}"
+      document_view_count_anonymous = Analytics.get_keen_data(query_type: 'count', collection: 'anonymous_document_views')
+      @doc_views_anonymous = "Document views, anonymous: #{document_view_count_anonymous}"
+
+      edits = Analytics.get_keen_data(query_type: 'count', collection: 'document_edit')
+      @edits = "Edits: #{edits}"
 
       sign_ins_count = Analytics.get_keen_data(query_type: 'count', collection: 'sign_ins')
-      sign_ins_average = Analytics.get_keen_data(query_type: 'average', collection: 'sign_ins')
-      @analytics_sign_ins = "Sign ins: #{sign_ins_count}, average: #{sign_ins_average}"
+      @sign_ins = "Sign ins: #{sign_ins_count}"
 
       sign_outs_count = Analytics.get_keen_data(query_type: 'count', collection: 'sign_outs')
-      sign_outs_average = Analytics.get_keen_data(query_type: 'average', collection: 'sign_outs')
-      @analytics_sign_outs = "Sign outs: #{sign_outs_count}, average: #{sign_outs_average}"
+      @sign_outs = "Sign outs: #{sign_outs_count}"
 
       sign_ups_count = Analytics.get_keen_data(query_type: 'count', collection: 'sign_ups')
-      sign_ups_average = Analytics.get_keen_data(query_type: 'average', collection: 'sign_ups')
-      @analytics_sign_ups = "Sign ups: #{sign_ups_count}, average: #{sign_ups_average}"
+      @sign_ups = "Sign ups: #{sign_ups_count}"
 
       unauth_access_attempt_count = Analytics.get_keen_data(query_type: 'count', collection: 'unauthorized_access_attempt')
-      unauth_access_attempt_average = Analytics.get_keen_data(query_type: 'average', collection: 'sign_ups')
-      @analytics_unauth_access_attempts = "Unauthorized access attempts: #{unauth_access_attempt_count}, average: #{unauth_access_attempt_average}"
+      @unauth_access_attempts = "Unauthorized access attempts: #{unauth_access_attempt_count}"
     end
   end
 end
