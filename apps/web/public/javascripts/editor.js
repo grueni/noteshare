dd6ccebd0b0ccf4b03c75d74eb6a495a53d361d6
James Carlson <jxxcarlson@mac.com>
2016-01-25 04:57:27 -0700

    Click on image id inthe tool br ofteh editor to insert the uploadedimage.

--- apps/web/public/javascripts/editor.js
+++ apps/web/public/javascripts/editor.js
@@ -35,6 +35,9 @@ $(document).ready(function(){
   });
 
 
+    $('#image_id_control').click(insert_image_link);
+
+
 });
 
 mark_text_as_dirty = function() {
@@ -123,4 +126,43 @@ count_words = function(text) {
 
   var message = "word count: " + text.split(" ").length
   return message
+}
+
+
+// http://stackoverflow.com/questions/15976574/how-to-add-a-text-to-a-textbox-from-the-current-position-of-the-pointer-with-jqu
+jQuery.fn.extend({
+    insertAtCaret: function(myValue){
+        return this.each(function(i) {
+            if (document.selection) {
+                //For browsers like Internet Explorer
+                this.focus();
+                sel = document.selection.createRange();
+                sel.text = myValue;
+                this.focus();
+            }
+            else if (this.selectionStart || this.selectionStart == '0') {
+                //For browsers like Firefox and Webkit based
+                var startPos = this.selectionStart;
+                var endPos = this.selectionEnd;
+                var scrollTop = this.scrollTop;
+                this.value = this.value.substring(0, startPos)+myValue+this.value.substring(endPos,this.value.length);
+                this.focus();
+                this.selectionStart = startPos + myValue.length;
+                this.selectionEnd = startPos + myValue.length;
+                this.scrollTop = scrollTop;
+            } else {
+                this.value += myValue;
+                this.focus();
+            }
+        })
+    }
+});
+
+
+insert_image_link = function() {
+
+    var text = 'image::'+ $('#current_image_id').html() + '[]'
+    $('#document-updated-text').insertAtCaret(text);
+    update_document();
+
 }
