674cc6f307bb3d073f938fd343af85ed07da307e
James Carlson <jxxcarlson@gmail.com>
2016-02-02 13:57:48 -0500

    add files for view source

---
+++ apps/web/controllers/documents/view_source.rb
@@ -0,0 +1,37 @@
+require 'keen'
+require_relative '../../../../lib/modules/analytics'
+
+module Web::Controllers::Documents
+  class ViewSource
+    include Web::Action
+    include Keen
+
+    expose :document, :root_document, :updated_text, :current_image,:active_item, :active_item2
+
+    def call(params)
+      puts "Enter: ViewSource".red
+      redirect_if_not_signed_in('web, document, ViewSource')
+      user = current_user(session)
+      id = params['id']
+
+      if session['current_image_id']
+        @current_image = ImageRepository.find session['current_image_id']
+      end
+
+      @active_item = 'reader'
+      @active_item2 = 'source'
+      @document = DocumentRepository.find(id)
+      @root_document = @document.root_document
+      Analytics.record_document_view(user, @document)
+      session[:current_document_id] = id
+
+      cm = ContentManager.new(@document)
+      if @document.is_root_document?
+        cm.compile_with_render_lazily
+      else
+        cm.update_content_lazily
+      end
+      @updated_text = @document.content
+    end
+  end
+end
---
+++ apps/web/views/documents/view_source.rb
@@ -0,0 +1,5 @@
+module Web::Views::Documents
+  class ViewSource
+    include Web::View
+  end
+end
