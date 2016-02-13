437e7ecbf3a79021e695d7aecc8eab820194a528
James Carlson <jxxcarlson@mac.com>
2016-02-12 21:32:19 -0500

    remove commented out code

--- config/puma.rb
+++ config/puma.rb
@@ -14,26 +14,3 @@ on_worker_boot do
 end
 
 
-=begin
-
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
-
-preload_app!
-
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || ENV['LOTUS_ENV'] || 'development'
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  #  ActiveRecord::Base.establish_connection
-end
-
-
-# workers Integer(ENV['PUMA_WORKERS'] || 3)
-
-
-=end
