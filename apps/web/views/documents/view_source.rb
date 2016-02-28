880f4236b594324520a2ee9085b414d75b36ba55
James Carlson <jxxcarlson@mac.com>
2016-02-28 08:49:00 -0800

    Use editor form in template for view source but do not connect it to any action.

--- apps/web/views/documents/view_source.rb
+++ apps/web/views/documents/view_source.rb
@@ -6,5 +6,17 @@ module Web::Views::Documents
       DocumentPresenter.new(document)
     end
 
+    def form
+      puts "form EDITOR".red
+
+      form_for :document, '/editor/update' do
+
+        text_area :updated_text,  document.content, { class: 'editor_input',  style: 'position: absolute; top:0'}
+
+        hidden_field :document_id, value: document.id
+
+      end
+    end
+
   end
 end
