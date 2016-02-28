d2f07a7b720132c3c21087ea2981905651161923
James Carlson <jxxcarlson@mac.com>
2016-02-27 22:13:13 -0800

    In class Link, derive @prefix from referer

--- apps/web/controllers/document/link.rb
+++ apps/web/controllers/document/link.rb
@@ -2,18 +2,25 @@ module Web::Controllers::Document
   # Class link redirects an income request for a document
   # to a request for the same document prefixed by the
   # user's screen_name.  This is to ensure continuity
-  # of experience.
+  # of user experience.
   class Link
     include Web::Action
 
     def configure_prefix
-      @prefix = 'www'
-      cu = current_user(session)
-      @prefix = cu.screen_name if cu
+      referer = request.env["HTTP_REFERER"]
+      part = referer.sub(/https?:\/\//, '').split('/')[0].split('.')
+      prefix = part.shift
+      stem = part.join('.')
+      if ".#{stem}" == ENV['DOMAIN']
+        @prefix = prefix
+      else
+        @prefix = 'www'
+      end
     end
-    
+
     def parse_query_string
       @stem = request.query_string || 'document'
+      @stem = 'document' if ['document', 'aside', 'view_source', 'compiled', 'titlepage'].include? @stem
     end
 
     def new_link(params)
