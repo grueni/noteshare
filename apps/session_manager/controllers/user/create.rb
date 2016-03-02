4a298f7caf3de0f25e81add1bf986a1bba07e9f5
James Carlson <jxxcarlson@mac.com>
2016-03-02 14:52:16 -0500

    - Put in validations and redirects for signups - Redirect to start page on sign up

--- apps/session_manager/controllers/user/create.rb
+++ apps/session_manager/controllers/user/create.rb
@@ -12,22 +12,49 @@ module SessionManager::Controllers::User
 
     def call(params)
 
+      puts params[:user]
+
       email =  params[:user]['email']
+      redirect_to '/error/1?You must enter an email address' if email == ''
+      redirect_to '/error/1?You must enter a VALID email address' if !(email =~ /\A[a-zA-Z].*@[a-zZ-Z].*\.[a-zA-Z]*/)
       user =  UserRepository.find_one_by_email email
       redirect_to '/error/1?The email you entered is already taken' if user
 
-      new_user = User.create(params[:user])
 
-      new_user.first_name = new_user.first_name || ''
-      new_user.first_name = new_user.first_name.strip.capitalize
+      first_name = (params[:user]['first_name'] || '').strip.capitalize
+      if first_name == ''
+        redirect_to '/error/1?Your did not fill in your first name'
+      end
 
-      new_user.last_name = new_user.last_name || ''
-      new_user.last_name = new_user.last_name.strip.capitalize
 
-      new_user.screen_name = new_user.screen_name || ''
-      new_user.screen_name = new_user.screen_name .strip.downcase
+      last_name = (params[:user]['last_name'] || '').strip.capitalize
+      if last_name == ''
+        redirect_to '/error/1?Your did not fill in your last name'
+      end
+
+      screen_name = params[:user]['screen_name'] || ''
+      puts "screen_name length: #{screen_name.length}".red
+      if screen_name.length < 4 or screen_name =~ / /
+        redirect_to '/error/1?Your screen name must have at least 4 characters with no spaces'
+      end
+      screen_name = screen_name.downcase
+
+      user2 =  UserRepository.find_one_by_screen_name screen_name
+      redirect_to '/error/1?The screen_name you entered is already taken' if user2
 
       password = params[:user]['password']
+      puts "password: #{password}".red
+      redirect_to '/error/1?You must enter a password.' if password == ''
+      redirect_to '/error/1?Your password must have at least 8 characters.' if password.length < 8
+
+      password_confirmation = params[:user]['password_confirmation']
+      redirect_to '/error/1?You must enter a password_confirmation.' if password_confirmation == ''
+      puts "password_confirmatin: #{password_confirmation}".red
+      if password != password_confirmation
+        redirect_to '/error/1?The passwords did not match' if user2
+      end
+
+      new_user = User.create(first_name: first_name, last_name: last_name, screen_name: screen_name, password: password, password_confirmation: password_confirmation)
 
       if new_user
         puts "app = SessionManager, controller = Create (User)".red
@@ -37,7 +64,7 @@ module SessionManager::Controllers::User
         UserRepository.update new_user
 
         token = params[:user]['token']
-        if token
+        if token != ''
           cp = CommandProcessor.new(user: new_user, token: token)
           cp.execute
         end
@@ -47,7 +74,7 @@ module SessionManager::Controllers::User
 
       if new_user
         Keen.publish(:sign_ups, { :username => new_user.screen_name })
-        redirect_to  basic_link new_user.node_name, "node/user/#{new_user.id}"
+        redirect_to  basic_link new_user.node_name, "node/34"
       else
         redirect_to '/error/2?Something went wrong with the signup process'
       end
