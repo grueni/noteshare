0df20fa0303f55cae8f6f491b77526d4d124d23f
James Carlson <jxxcarlson@mac.com>
2016-03-03 15:17:13 -0500

    c

--- apps/editor/controllers/document/export_latex.rb
+++ apps/editor/controllers/document/export_latex.rb
@@ -5,11 +5,13 @@ module Editor::Controllers::Document
     def call(params)
       id = params[:id]
       document = DocumentRepository.find id
+      document = document.root_document
       redirect_to "/error:#{id}?Document not found" if document == nil
       e = Exporter.new(document)
       e.export_latex
       output = "<p style='margin:3em;'> <strong>#{document.title}</strong> exported as Asciidoc and LaTeX to "
       output << "<a href='http://vschool.s3.amazonaws.com/latex/#{document.id}.tar'>this link</a> "
+      output << "<p style='margin:3em;'> The file to download from the link is #{document.id}.tar</p>"
       output << "</p>\n\n"
       self.body = output
     end
