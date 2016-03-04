13eaa2d74e7daf02c4b5e6cb165a09a0a974fc10
James Carlson <jxxcarlson@mac.com>
2016-03-04 12:03:51 -0500

    fix nesting error in call

--- apps/node/controllers/admin/edit_sidebar.rb
+++ apps/node/controllers/admin/edit_sidebar.rb
@@ -5,26 +5,24 @@ module Node::Controllers::Admin
     expose :active_item, :node
 
     def call(params)
-
-      def call(params)
-        puts "Edit sidebar call".red
-        cu = current_user(session)
-        if cu
-          puts "  -- current_user: #{cu.full_name}".cyan
-        else
-          puts "  -- no current_user".cyan
-        end
-        redirect_if_not_signed_in('image, Admin,  Edit')
-        @active_item='admin'
-        id = params['id']
-        @node = NSNodeRepository.find(id)
-        #fixme:
-
-        if @node.meta == nil
-          @node.meta = {}
-          NSNodeRepository.update @node
-        end
+      puts "Edit sidebar call".red
+      cu = current_user(session)
+      if cu
+        puts "  -- current_user: #{cu.full_name}".cyan
+      else
+        puts "  -- no current_user".cyan
       end
+      redirect_if_not_signed_in('image, Admin,  Edit')
+      @active_item='admin'
+      id = params['id']
+      @node = NSNodeRepository.find(id)
+      #fixme:
+
+      if @node.meta == nil
+        @node.meta = {}
+        NSNodeRepository.update @node
+      end
+
     end
   end
 end
