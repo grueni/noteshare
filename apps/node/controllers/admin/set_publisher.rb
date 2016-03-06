534a411796137162d2bba2c17b3789af61a32a65
James Carlson <jxxcarlson@mac.com>
2016-03-06 12:48:28 -0500

    Add SetPublisher controller

---
+++ apps/node/controllers/admin/set_publisher.rb
@@ -0,0 +1,12 @@
+module Node::Controllers::Admin
+  class SetPublisher
+    include Node::Action
+
+    def call(params)
+      redirect_if_not_admin('node, Admin,  SetPublisher')
+      node = NSNodeRepository.find params[:id]
+      node.make_all_documents_principal
+      self.body = 'DONE!'
+    end
+  end
+end
