e7a4f755ad231f9b1136deb9c07a537a17326d37
James Carlson <jxxcarlson@mac.com>
2016-03-07 00:51:34 -0500

    reverse user list sorted by date

--- apps/admin/controllers/users/list.rb
+++ apps/admin/controllers/users/list.rb
@@ -7,7 +7,7 @@ module Admin::Controllers::Users
     def call(params)
       redirect_if_not_admin('Attempt to list users (admin, users, list)')
       @active_item = 'admin'
-      @users = UserRepository.all.sort_by{ |user| user.updated_at }
+      @users = UserRepository.all.sort_by{ |user| user.updated_at }.reverse
     end
 
   end
