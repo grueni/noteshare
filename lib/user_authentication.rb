c3145dbebc7d61249ff76b1fc8cff6b5125a32bb
James Carlson <jxxcarlson@mac.com>
2016-03-08 17:48:05 -0500

    Also initiaize session['current_document_id'] on login

--- lib/user_authentication.rb
+++ lib/user_authentication.rb
@@ -32,12 +32,12 @@ class UserAuthentication
       session[:current_document_id] = @user.dict2['current_document_id']
       session[:current_image_id] = @user.dict2['current_image_id']
       session[:domain] = "#{@user.node_name}#{ENV['DOMAIN']}"
-      puts "At login, user = #{@user.full_name}".red
       @user.dict2['show_overlay'] = 'yes' if  @user.dict2['show_overlay'] == nil
       @user.dict2['show_overlay_this_session'] = 'yes' unless @user.dict2['show_overlay'] == 'no'
       @user.dict2['search_scope'] = 'all'
       @user.dict2['search_mode'] = 'document'
       @user.dict2['current_document_id'] = ENV['GETTING_STARTED_ID'] if @user.dict2['current_document_id'] == nil
+      session['current_document_id'] = @user.dict2['current_document_id']
       UserRepository.update @user
       return @user
     else
