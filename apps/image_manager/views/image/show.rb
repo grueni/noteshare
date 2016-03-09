c8ed4c80f4cf205c7441be2386ec3745058534ce
James Carlson <jxxcarlson@mac.com>
2016-03-09 08:15:08 -0500

    Use view to construct form for template

--- apps/image_manager/views/image/show.rb
+++ apps/image_manager/views/image/show.rb
@@ -2,5 +2,25 @@ module ImageManager::Views::Image
   class Show
     include ImageManager::View
     include Lotus::Helpers
+
+    def form
+
+      form_for :edit_image, "/image_manager/update/#{image.id}", class: '' do
+
+        label :title
+        text_field :title, image.title
+
+        label :tags
+        text_field :tags, image.tags
+
+        label :source
+        text_field :source, image.source
+
+        submit 'Update',  class: "waves-effect waves-light btn"
+
+      end
+    end
+
+
   end
 end
