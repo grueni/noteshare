2310c552c6f6580ea2179d93d2c6d64ad4f1cb14
James Carlson <jxxcarlson@mac.com>
2016-03-10 00:40:04 -0500

    move command_input_form from layout back to home index

--- apps/admin/views/home/index.rb
+++ apps/admin/views/home/index.rb
@@ -2,6 +2,13 @@ module Admin::Views::Home
   class Index
     include Admin::View
 
+    def command_input_form
+      form_for :command_processor, '/admin/process_command' do
+        text_field :command, {id: 'command_form', style: 'margin-left:0;'}
+        hidden_field :secret_token, value: ENV['COMMAND_SECRET_TOKEN'], placeholder: 'command'
+        submit 'Execute',  class: "waves-effect waves-light btn", style: 'margin-top:3em;'
+      end
+    end
 
   end
 end
