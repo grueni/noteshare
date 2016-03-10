b1658f2c365cc14526511a53feb806196c90ce06
James Carlson <jxxcarlson@mac.com>
2016-03-10 08:56:06 -0500

    fix admin info bug

--- apps/admin/views/application_layout.rb
+++ apps/admin/views/application_layout.rb
@@ -7,15 +7,6 @@ module Admin
 
 
 
-      def command_input_form
-        form_for :command_processor, '/admin/process_command' do
-          text_field :command, {id: 'command_form', style: 'margin-left:0;'}
-          hidden_field :secret_token, value: ENV['COMMAND_SECRET_TOKEN'], placeholder: 'command'
-          submit 'Execute',  class: "waves-effect waves-light btn", style: 'margin-top:3em;'
-        end
-
-
-
       #####################################################
       #
       #   4. ADMIN LINKS
@@ -67,4 +58,4 @@ module Admin
       end
     end
   end
-end
+
