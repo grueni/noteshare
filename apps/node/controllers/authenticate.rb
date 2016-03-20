4bf73ac6fb3240e21988f34fd9ede50422b3221e
James Carlson <jxxcarlson@mac.com>
2016-03-20 08:16:00 -0600

    Fix logic errors in node authenticate module

--- apps/node/controllers/authenticate.rb
+++ apps/node/controllers/authenticate.rb
@@ -10,10 +10,25 @@ module Node
 
     private
 
+
+    # do not require authentication for routes like
+    # '/node/math' or /node/123' where the second
+    # element is an id.  Do require it if the
+    # second element is on a defined list, e.g., 'admin'
+    # Require authentication for all others.
     def authenticate!
       # puts request.env.keys
       request_path = request.env['REQUEST_PATH']
-      halt 401 unless request_path =~ /node\/public/ or authenticated?
+      request_path_parts = (request_path.split '/').select{ |x| x.length > 0 }
+      stem = request_path_parts[1] if request_path_parts.count > 1
+      pass = authenticated?
+      if request_path_parts.count == 2
+        if ['admin', 'settings', 'new', 'create'].include? stem
+          halt 401 unless pass
+        end
+      else
+        halt 401 unless pass
+      end
     end
 
     def authenticated?
