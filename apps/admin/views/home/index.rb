d8088e8e1def1269af07d5cae8b2e2ab32a2c928
James Carlson <jxxcarlson@mac.com>
2016-03-21 17:11:53 -0600

    Put placeholders in search and command fields

--- apps/admin/views/home/index.rb
+++ apps/admin/views/home/index.rb
@@ -10,7 +10,7 @@ module Admin::Views::Home
 
     def command_input_form
       form_for :command_processor, '/admin/process_command' do
-        text_field :command, {id: 'command_form', style: 'margin-left:0;'}
+        text_field :command, {id: 'command_form', style: 'margin-left:0;', placeholder: 'Command ...'}
         hidden_field :secret_token, value: ENV['COMMAND_SECRET_TOKEN'], placeholder: 'command'
         submit 'Execute',  class: "waves-effect waves-light btn", style: 'margin-top:3em;'
       end
