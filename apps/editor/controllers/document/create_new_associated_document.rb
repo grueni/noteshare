3d0d8fa4dc140506471a79ea16a7d0fc14ae2d96
James Carlson <jxxcarlson@mac.com>
2016-03-21 03:54:05 -0600

    Move guts of controller add associated document to interactor

--- apps/editor/controllers/document/create_new_associated_document.rb
+++ apps/editor/controllers/document/create_new_associated_document.rb
@@ -1,3 +1,5 @@
+require_relative '../../../../lib/noteshare/interactors/create_associated_document'
+
 module Editor::Controllers::Document
   class CreateNewAssociatedDocument
     include Editor::Action
@@ -6,44 +8,12 @@ module Editor::Controllers::Document
 
     def call(params)
       @active_item = 'editor'
-      puts 'controller: CreateNewAssociatedDocument'.red
-
-      document_packet = params['document']
-      title = document_packet['title']
-      if title == nil or title == ''
-        redirect_to '/error/:0?Please enter a title for the new document'
+      result = CreateAssociatedDocument.new(params).call
+      if result.error
+        redirect_to "/error/0?#{result.error}"
+      else
+        redirect_to "/editor/document/#{result.current_document.root_document.id}"
       end
-
-      type = document_packet['type']
-      if type == nil or type == ''
-        redirect_to '/error/:0?Please enter a type e.g., note, aside, or texmacro'
-      end
-
-      content = document_packet['content']
-      current_document_id = document_packet['current_document_id']
-
-      type = 'note' if type == ''
-
-      current_document = DocumentRepository.find current_document_id
-
-      author_id = current_document.author_credentials2['id']
-      author = UserRepository.find author_id
-      new_document = NSDocument.create(title: title, content: content, author_credentials: author.credentials)
-
-      #Fixme: the following is to be deleted when author_id is retired
-      author_id = current_document.author_credentials2['id']
-      new_document.author_id = author_id.to_i
-
-      new_document.acl = current_document.root_document.acl
-
-      ContentManager.new(new_document).update_content(nil)
-
-      DocumentRepository.update new_document
-      # AssociateDocManagerX
-      new_document.associate_to(current_document, type)
-
-      redirect_to "/editor/document/#{current_document.root_document.id}"
-
     end
 
   end
---
+++ lib/noteshare/interactors/create_associated_document.rb
@@ -0,0 +1,45 @@
+require 'lotus/interactor'
+
+class CreateAssociatedDocument
+
+  include Lotus::Interactor
+  expose :current_document, :error
+
+  def initialize(params)
+    document_packet = params['document']
+    @title = document_packet['title']
+    @type = document_packet['type']
+    @type = 'note' if @type == ''
+    @content = document_packet['content']
+    @current_document_id = document_packet['current_document_id']
+    @current_document = DocumentRepository.find @current_document_id
+    @author_id = @current_document.author_credentials2['id']
+    @author = UserRepository.find @author_id
+    @error = nil
+  end
+
+  def validate
+
+    if @title == nil or @title == ''
+      @error = '/error/:0?Please enter a title for the new document'
+      return
+    end
+
+    if @type == nil or type == ''
+      @error = '/error/:0?Please enter a type e.g., note, aside, or texmacro'
+      return
+    end
+
+  end
+
+  def call
+    @new_document = NSDocument.create(title: @title, content: @content, author_credentials: @author.credentials)
+    #Fixme: the following is to be deleted when author_id is retired
+    @new_document.author_id = @author.id
+    @new_document.acl = @current_document.root_document.acl
+    ContentManager.new(@new_document).update_content(nil)
+    DocumentRepository.update @new_document
+    @new_document.associate_to(@current_document, @type)
+  end
+
+end
