55b69d0e93131a98c14913e3997b300aafc187d7
James Carlson <jxxcarlson@gmail.com>
2016-03-31 12:48:40 -0400

    Tweaks to search scop and mode initializatin and persistence.

--- apps/web/controllers/documents/set_search_type.rb
+++ apps/web/controllers/documents/set_search_type.rb
@@ -8,15 +8,17 @@ module Web::Controllers::Documents
 
       search_scope_matcher = data.match(/search_scope=(.*?)&/)
       search_scope =  search_scope_matcher[1] if  search_scope_matcher
+      puts "SST: search_scope = #{search_scope}".red
 
       search_mode_matcher = data.match(/search_mode=(.*?)&/)
       search_mode = search_mode_matcher[1] if search_mode_matcher
+      puts "SST: search_mode = #{search_mode}".red
 
       puts "I AM SETTING THE SEARCH MODE TO #{search_mode}".magenta
 
       if current_user2
-        current_user2.dict2['search_scope'] = search_scope if search_scope
-        current_user2.dict2['search_mode'] = search_mode if search_mode
+        current_user2.dict2['search_scope'] = search_scope || 'local'
+        current_user2.dict2['search_mode'] = search_mode || 'document'
         UserRepository.update current_user2
       end
 
