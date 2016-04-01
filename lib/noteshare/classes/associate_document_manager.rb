f0bc36252700906b1802bf4ba9c5d3f00f0ff018
James Carlson <jxxcarlson@gmail.com>
2016-03-31 22:40:00 -0400

    Begin construction of AssociateDocumentManager class

--- lib/noteshare/classes/associate_document_manager.rb
+++ lib/noteshare/classes/associate_document_manager.rb
@@ -1,84 +1,22 @@
+# The AssociateDocumentManager class
+# is responsible for adding, manipulating,
+# and deleteing associates documents
+class AssociateDocumentManager
 
-class AssociateDocManager
-
-
-  def initialize(document)
-    @document = document
+  def initialize(parent_document)
+    @parent_document = parent_document
   end
 
-
-
-  ############################################################
-
-  # EXTERNAL (3)
-  def root_associated_document_map(target='reader')
-    root = @document.root_document || @document
-    if root != @document
-      adm = AssociateDocManager.new(root)
-      adm.associated_document_map(target)
-    else
-      associated_document_map(target)
-    end
+  # @foo.associate_to(@bar, 'summary',)
+  # associates @foo to @bar as a 'summary'.
+  # It can be retrieved as @foo.associated_document('summary')
+  def associate(document, type)
+    @parent_document.doc_refs[type] = self.id
+    document.type = 'associated:' + type
+    document.set_parent_document_to(@parent_document)
+    document.set_root_document_to(@parent_document)
+    DocumentRepository.update(@parent_document)
+    DocumentRepository.update(document)
   end
-
-  # Remove stale keys
-  # Fixme: this will become obsolete
-  # when things are working better
-  def heal_associated_docs
-    bad_keys = []
-    @document.doc_refs do |key, value|
-      if DocumentRepository.find key == nil
-        bad_keys << key
-      end
-    end
-    bad_keys.each do |key|
-      @document.doc_refs.delete(key)
-    end
-    DocumentRepository.update @document
-  end
-
-  # ONE INTERNAL USE
-  def associated_document_map(target='reader')
-
-    heal_associated_docs
-
-    if @document.type =~ /associated:/
-      document = @document.parent_document
-    else
-      document = @document
-    end
-
-    hash = document.doc_refs
-    keys = hash.keys
-    if keys
-      keys.delete 'previous'
-      keys.delete 'next'
-      keys.delete 'index'
-      map = "<ul>\n"
-      keys.sort.each do |key|
-        if target == 'editor'
-          map << '<li>' << "#{associate_link(key, 'editor')}</li>\n"
-        else
-          map << '<li>' << "#{associate_link(key)}</li>\n"
-        end
-      end
-      map << "</ul>\n"
-    else
-      map = ''
-    end
-    map
-  end
-
-
-  # INTERNAL
-  def associate_link(type, prefix='')
-    if prefix == ''
-      "<a href='/document/#{@document.doc_refs[type]}'>#{type.capitalize}</a>"
-    else
-      "<a href='/#{prefix}/document/#{@document.doc_refs[type]}'>#{type.capitalize}</a>"
-    end
-
-  end
-
 
 end
