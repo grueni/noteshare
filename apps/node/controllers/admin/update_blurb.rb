0a77643bb42bacce57aac9097e7e27a7580c1e55
James Carlson <jxxcarlson@gmail.com>
2016-04-01 08:42:16 -0400

    Refactor update_blurb using interactor

--- apps/node/controllers/admin/update_blurb.rb
+++ apps/node/controllers/admin/update_blurb.rb
@@ -1,30 +1,15 @@
+require_relative '../../../../lib/noteshare/interactors/edit_node_blurb'
+
 module Node::Controllers::Admin
   class UpdateBlurb
     include Node::Action
 
     def call(params)
-
-      data = params['node']
-
-      puts "data: #{data.inspect}".red
-      blurb_text = data['blurb'] || ''
-
-      puts "blurb_text: #{blurb_text}".cyan
-
-      id = params['id']
-      node = NSNodeRepository.find id
-      node.meta['long_blurb'] = blurb_text
-
-      # @renderer = Render.new(blurb_text)
-      # node.meta['rendered_blurb'] = @renderer.convert
-      # node.meta['long_blurb'] = blurb_text
-      node.update_blurb
-
-      NSNodeRepository.update node
-
-      redirect_to basic_link current_user2.screen_name, "node/#{id}"
-
-      self.body = "OK Boss, I am updating the blurb, just as you say.: #{blurb_text}"
+      blurb_text = params['node']['blurb'] || ''
+      node_id =params['id']
+      EditNodeBlurb.new(node_id, blurb_text ).call
+      redirect_to basic_link current_user2.screen_name, "node/#{node_id}"
     end
+    
   end
 end
