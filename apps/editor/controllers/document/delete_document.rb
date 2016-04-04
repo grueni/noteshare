966c230459909b8d82646f0c87c7c3c4fd5324be
James Carlson <jxxcarlson@gmail.com>
2016-04-04 09:26:01 +0800

    Refactor editor delete with interactor

--- apps/editor/controllers/document/delete_document.rb
+++ apps/editor/controllers/document/delete_document.rb
@@ -1,47 +1,16 @@
-  module Editor::Controllers::Document
+require_relative '../../../../lib/noteshare/interactors/editor/delete_ns_document'
+
+module Editor::Controllers::Document
   class DeleteDocument
     include Editor::Action
 
     expose :active_item
 
     def call(params)
-
       @active_item = 'editor'
-      control =  params['document']['destroy']
-      doc_id =  params[:id]
-      @delete_mode = params['document']['delete_mode']
-      @document = DocumentRepository.find doc_id
-
-      parent_id = @document.parent_document.id if @document.parent_document
-      document_id = @document.id
-
-
-      redirect_to '/error/666' if Permission.new(current_user2, :delete,  @document).grant == false
-      message = ''
-
-      if control != 'destroy'
-        message << "You did not say 'destroy'"
-        redirect_to "/editor/document/#{doc_id}"
-      end
-
-
-      if @delete_mode == 'section'
-
-        @document.delete
-        message << "#{@document.title} has been deleted."
-        if parent_id != document_id
-          redirect_to "/editor/document/#{parent_id}"
-        else
-          redirect_to "/node/user/#{current_user2.id}"
-        end
-      end
-
-      if @delete_mode == 'tree'
-        DocumentRepository.destroy_tree @document.id, [:verbose, :kill]
-        message << "#{@document.title} and it entrie document tree has been deleted."
-        redirect_to "/node/user/#{current_user2.id}"
-      end
-
+      result = DeleteNSDocument.new(params, current_user2).call
+      redirect_to result.redirect_path
     end
+
   end
 end
---
+++ lib/noteshare/interactors/editor/delete_ns_document.rb
@@ -0,0 +1,55 @@
+require 'lotus/interactor'
+
+class DeleteNSDocument
+
+  include Lotus::Interactor
+  expose :document, :root_document, :error, :message, :redirect_path
+
+  def initialize(params, user)
+    @user = user
+    @control =  params['document']['destroy']
+    doc_id =  params[:id]
+    @delete_mode = params['document']['delete_mode']
+    @document = DocumentRepository.find doc_id
+    @error = nil
+    @message = ''
+  end
+
+  def call
+
+    parent_id = @document.parent_document.id if @document.parent_document
+    document_id = @document.id
+
+    if Permission.new(@user, :delete,  @document).grant == false
+      @error = 'permission to delete document not granted'
+      @redirect_path =  "/error/0?#{@error}"
+      return
+    end
+
+
+    if @control != 'destroy'
+      @message << "You did not say 'destroy'"
+      @error = 'deletion of documnt not confirmed'
+      @redirect_path =  "/error/0?#{@error}"
+      return
+    end
+
+    if @delete_mode == 'section'
+      @document.delete
+      @message << "#{@document.title} has been deleted."
+      if parent_id != document_id
+        @redirect_path =  "/editor/document/#{parent_id}"
+      else
+        @redirect_path = "/node/user/#{@user.id}"
+      end
+    end
+
+    if @delete_mode == 'tree'
+      DocumentRepository.destroy_tree @document.id, [:verbose, :kill]
+      @message << "#{@document.title} and it entrie document tree has been deleted."
+      @redirect_path = "/node/user/#{@user.id}"
+    end
+  end
+
+
+end
