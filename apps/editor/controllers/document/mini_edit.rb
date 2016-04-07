1c5bdc94d9efb1a33ecb338a92d7c4fbbe7634a5
James Carlson <jxxcarlson@gmail.com>
2016-04-07 13:54:35 +0800

    Add mini_edit files to repo

---
+++ apps/editor/controllers/document/mini_edit.rb
@@ -0,0 +1,30 @@
+module Editor::Controllers::Document
+  class MiniEdit
+    include Editor::Action
+
+    expose :document, :parent_document, :root_document, :updated_text,
+           :current_image, :active_item, :editors
+
+    def call(params)
+      @active_item = 'editor'
+      id = params['id']
+
+      result = EditDocument.new(params, current_user2).call
+      @document = result.document
+      @root_document= result.root_document
+      @parent_document = @document.parent_document
+      @updated_text = result.updated_text
+      @editors = result.editors
+
+      session['current_document_id'] = @document.id
+
+      if session['current_image_id']
+        @current_image = ImageRepository.find session['current_image_id']
+      end
+
+      # self.body = "HOLA! id = #{id}"
+
+    end
+
+  end
+end
---
+++ apps/editor/views/document/mini_edit.rb
@@ -0,0 +1,27 @@
+module Editor::Views::Document
+  class MiniEdit
+    include Editor::View
+
+    def form
+      puts "form EDITOR".red
+
+      form_for :document, '/editor/update' do
+
+        text_area :updated_text,  document.content, { class: 'editor_input',  style: 'position: absolute; top:0'}
+
+        hidden_field :document_id, value: document.id
+
+      end
+    end
+
+
+    def document_presenter
+      DocumentPresenter.new(document)
+    end
+
+    def toc_presenter
+      TOCPresenter.new(document.root_document)
+    end
+
+  end
+end
