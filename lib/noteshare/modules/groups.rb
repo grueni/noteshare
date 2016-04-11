9927fd90deb7bff2a47e8c0b3fbf6e956e6579f7
James Carlson <jxxcarlson@gmail.com>
2016-04-11 22:22:11 +0800

    Put module Groups in Notehare::Helpers

--- lib/noteshare/modules/groups.rb
+++ lib/noteshare/modules/groups.rb
@@ -1,44 +1,44 @@
 module Noteshare
+  module Helpers
+    module Groups
 
-  module Groups
+      ########## GROUPS   #############
+      #
+      # Add groups behavior, e.g, to NSDocument.
+      # requirements: a field groups_json of type jsommb
+      #
 
-    ########## GROUPS   #############
-    #
-    # Add groups behavior, e.g, to NSDocument.
-    # requirements: a field groups_json of type jsommb
-    #
-
-    def groups
-      data = groups_json || "[]"
-      JSON.parse(data)
-    end
-
-    def set_groups(array)
-      self.groups_json = array.to_json
-      DocumentRepository.update self
-    end
-
-    def add_group(group)
-      _groups = self.groups || []
-      if (_groups.include? group) == false
-        _groups << group
-        self.set_groups _groups
+      def groups
+        data = groups_json || "[]"
+        JSON.parse(data)
       end
-    end
 
-    def has_group(group)
-      puts self.groups
-      return self.groups.include? group
-    end
-
-    def delete_group(group)
-      _groups = self.groups
-      if (_groups.include? group)
-        _groups.delete group
-        self.set_groups _groups
+      def set_groups(array)
+        self.groups_json = array.to_json
+        DocumentRepository.update self
       end
-    end
 
+      def add_group(group)
+        _groups = self.groups || []
+        if (_groups.include? group) == false
+          _groups << group
+          self.set_groups _groups
+        end
+      end
+
+      def has_group(group)
+        puts self.groups
+        return self.groups.include? group
+      end
+
+      def delete_group(group)
+        _groups = self.groups
+        if (_groups.include? group)
+          _groups.delete group
+          self.set_groups _groups
+        end
+      end
+
+    end
   end
-
 end
--- spec/noteshare/modules/group_spec.rb
+++ spec/noteshare/modules/group_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+include Noteshare::Core::Document
+include Noteshare::Helpers::Groups
 
 describe Groups do
 
