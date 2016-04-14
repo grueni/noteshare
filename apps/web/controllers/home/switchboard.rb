e8b4ba2bf81bd2d56010bb22e30c52f50c348b48
James Carlson <jxxcarlson@gmail.com>
2016-04-14 09:08:03 +0800

    Factor an interactor out of the switchboard controller.

--- apps/web/controllers/home/switchboard.rb
+++ apps/web/controllers/home/switchboard.rb
@@ -3,69 +3,19 @@ require_relative '../../../../lib/ui/links'
 
 module Web::Controllers::Home
 
-
   class Switchboard
     include Web::Action
-    include UI::Links
     include Noteshare::Core::Node
 
     expose :active_item
 
-
-    def host_base
-      http_host_parts = request.env['HTTP_HOST'].split('.')
-      if http_host_parts[-1] =~ /localhost/
-        return http_host_parts[-1]
-      else
-        return "#{http_host_parts[-2]}.#{http_host_parts[-1]}"
-      end
-    end
-
-
-    # There are currently two values for the value of @node.type:
-    # 'public' and 'personal'
-    def handle_incoming_node
-
-      puts "handle_incoming_node".red
-
-      referer = request.env["HTTP_REFERER"]
-      puts "referer: #{referer}".cyan
-      @incoming_node = NSNode.from_http(request)
-      @incoming_node_name =  @incoming_node.name if @incoming_node
-      if @incoming_node
-        puts "@incoming_node: #{@incoming_node.name}".red
-      else
-        puts "No incoming node".red
-      end
-
-      if current_user2
-        prefix = current_user2.screen_name
-      else
-        prefix = @incoming_node_name || :none
-      end
-
-      if @incoming_node
-        if  @incoming_node.type == 'public'
-          # redirect_to "/node/#{@incoming_node.id}"
-          puts 'A'.magenta
-          redirect_to basic_link prefix, "node/#{@incoming_node.id}"
-        else
-          puts 'B'.magenta
-          redirect_to basic_link @incoming_node.name, "home"
-        end
-      else
-        puts 'C'.magenta
-        # redirect_to '/home/'
-        redirect_to basic_link :none, 'home'
-      end
-
-    end
-
-
     def call(params)
       @active_item = 'home'
-      handle_incoming_node
-
+      node = NSNode.from_http(request)
+      referer = request.env["HTTP_REFERER"]
+      hash = {user: current_user2, node: node, referer: referer}
+      result = Noteshare::Interactor::Web::Switchboard.new(hash).call
+      redirect_to result.redirect_path || '/'
     end
   end
 end
---
+++ lib/noteshare/interactors/web/switchboard.rb
@@ -0,0 +1,70 @@
+require 'lotus/interactor'
+
+module Noteshare
+  module Interactor
+    module Web
+      class Switchboard
+
+        include Lotus::Interactor
+        include Noteshare::Core::Node
+        include UI::Links
+
+        expose :redirect_path
+
+        def host_base
+          http_host_parts = request.env['HTTP_HOST'].split('.')
+          if http_host_parts[-1] =~ /localhost/
+            return http_host_parts[-1]
+          else
+            return "#{http_host_parts[-2]}.#{http_host_parts[-1]}"
+          end
+        end
+
+
+        # There are currently two values for the value of @node.type:
+        # 'public' and 'personal'
+        def handle_incoming_node
+          
+          puts "referer: #{@referer}".cyan
+
+          if @incoming_node
+            puts "@incoming_node: #{@incoming_node.name}".red
+          else
+            puts "No incoming node".red
+          end
+
+          if @user
+            prefix = @user.screen_name
+          else
+            prefix = @incoming_node_name || :none
+          end
+
+          if @incoming_node
+            if  @incoming_node.type == 'public'
+              @redirect_path = basic_link prefix, "node/#{@incoming_node.id}"
+            else
+              @redirect_path = basic_link @incoming_node.name, "home"
+            end
+          else
+            @redirect_path = basic_link :none, 'home'
+          end
+
+        end
+
+        def initialize(hash)
+          @user = hash[:user]
+          @incoming_node = hash[:node]
+          @incoming_node_name =  @incoming_node.name if @incoming_node
+          @referer = hash[:referer]
+        end
+
+        def call
+          handle_incoming_node
+        end
+
+      end
+
+    end
+  end
+end
+
