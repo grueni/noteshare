9f499e24b6414fa7ea1f50845aa172a2f207d085
James Carlson <jxxcarlson@gmail.com>
2016-04-15 09:39:08 +0800

    db classes >> module Database

--- lib/ext/pg_array.rb
+++ lib/ext/pg_array.rb
@@ -3,36 +3,40 @@ require 'lotus/model/coercer'
 require 'sequel'
 require 'sequel/extensions/pg_array'
 
-Sequel.extension :pg_array_ops
-Sequel.extension :pg_hstore_ops
+module Database
+
+  Sequel.extension :pg_array_ops
+  Sequel.extension :pg_hstore_ops
 
 
-class PGIntArray < Lotus::Model::Coercer
-  def self.dump(value)
-    ::Sequel.pg_array(value, :integer)
-  end
+  class PGIntArray < Lotus::Model::Coercer
+    def self.dump(value)
+      ::Sequel.pg_array(value, :integer)
+    end
 
-  def self.load(value)
-    if value
-      ::Kernel.Array(value)
-    else
-      []
+    def self.load(value)
+      if value
+        ::Kernel.Array(value)
+      else
+        []
+      end
     end
   end
-end
 
-class PGStringArray < Lotus::Model::Coercer
-  def self.dump(value)
-    ::Sequel.pg_array(value, :text)
-  end
+  class PGStringArray < Lotus::Model::Coercer
+    def self.dump(value)
+      ::Sequel.pg_array(value, :text)
+    end
 
-  def self.load(value)
-    if value
-      ::Kernel.Array(value)
-    else
-      []
+    def self.load(value)
+      if value
+        ::Kernel.Array(value)
+      else
+        []
+      end
     end
   end
-end
 
+
+end
 
--- lib/ext/pg_json.rb
+++ lib/ext/pg_json.rb
@@ -6,23 +6,28 @@ require 'lotus/model/coercer'
 require 'sequel'
 require 'sequel/extensions/pg_json'
 
-Sequel.extension :pg_json
-Sequel.extension :pg_json_ops
+module Database
+
+  Sequel.extension :pg_json
+  Sequel.extension :pg_json_ops
+
+  class PGJSONB  < Lotus::Model::Coercer
+
+    def self.dump(value)
+      puts "JSONB DUMP: #{value}".red
+      Sequel.pg_jsonb(value)
+    end
+
+    def self.load(value)
+      puts "JSONB LOAD: (#{value})".red
+      value
+    end
 
 
-
-class PGJSONB  < Lotus::Model::Coercer
-
-  def self.dump(value)
-    puts "JSONB DUMP: #{value}".red
-    Sequel.pg_jsonb(value)
   end
-
-  def self.load(value)
-    puts "JSONB LOAD: (#{value})".red
-    value
-  end
-
 
 end
+
+
+
 
