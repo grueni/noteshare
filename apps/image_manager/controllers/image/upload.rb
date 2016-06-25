bbb387219d9bc26fe5adbb283872837a0b859814
James Carlson <jxxcarlson@gmail.com>
2016-06-25 06:14:57 +0200

    enable image editor

--- apps/image_manager/controllers/image/upload.rb
+++ apps/image_manager/controllers/image/upload.rb
@@ -9,13 +9,13 @@ module ImageManager::Controllers::Image
       redirect_if_not_signed_in('editor, image,  Upload')
       @active_item = 'images'
       puts "ImageManager, Image. Upload".red
-=begin
+
       image_data = params['image']
       file_name = image_data['file_name']
       title = image_data['title']
       puts "title = #{title}, filename = #{file_name}".red
       message = Image.upload(file_name)
-=end
+
       self.body = "Hi there!" # message
     end
   end
